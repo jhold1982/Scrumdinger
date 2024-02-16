f6436978836bc00c5205464f110b6d6e0a57fccf
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-02-15 21:20:46 -0600

    Initial meeting view created

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct MeetingView: View {
+	
+	// MARK: - PROPERTIES
+	
+	
+	
+	// MARK: - VIEW BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		
+		
+		// MARK: - OUTER VSTACK
+		VStack {
+			ProgressView(value: 5, total: 15)
+			
+			HStack {
+				VStack(alignment: .leading) {
+					Text("Seconds Elapsed")
+						.font(.caption)
+					Label("300", systemImage: "hourglass.tophalf.fill")
+				}
+				
+				Spacer()
+				
+				VStack(alignment: .trailing) {
+					Text("Seconds Remaining")
+						.font(.caption)
+					Label("600", systemImage: "hourglass.bottomhalf.fill")
+				}
+			}
+			.accessibilityElement(children: .ignore)
+			.accessibilityLabel("Time remaining")
+			.accessibilityValue("10 minutes")
+			
+			Circle()
+				.strokeBorder(lineWidth: 24)
+			
+			HStack {
+				Text("Speaker 1 of 3")
+				Spacer()
+				Button(action: {}) {
+					Image(systemName: "forward.fill")
+				}
+				.accessibilityLabel("Next speaker")
+			}
+		}
+		.padding()
     }
 }
 
